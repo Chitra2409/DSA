2f6688c0bc4f646640f3cfb83909ea40690db5e6
Chitra2409 <107039511+Chitra2409@users.noreply.github.com>
2024-01-06 23:08:58 +0530

    1D array to 2D array conversion

---
+++ 1Dto2Darray.java
@@ -0,0 +1,21 @@
+//Question link - https://leetcode.com/problems/convert-1d-array-into-2d-array/
+
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        int result[][];
+        result = new int[m][n];
+         if (original.length != m * n) {
+            return new int[0][0];
+        }
+        int k = 0;
+        int totalElements = original.length;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n ; j++){
+            result[i][j] = original[k];
+            k++; 
+            }
+        }
+
+        return result;
+    }
+};
