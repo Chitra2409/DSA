5f710305815ecd03e362ec5f5b4461c7cfd89eb2
Chitra2409 <107039511+Chitra2409@users.noreply.github.com>
2024-02-19 16:57:23 +0530

    print decreasing numbers using recursion

--- printDecreasing.java
+++ printDecreasing.java
@@ -1,35 +1,19 @@
 package Recursion;
 import java.util.*;
-public class printDecreasingIncreasing {
+public class printDecreasing {
     public static void main(String args[]){
         Scanner sc= new Scanner(System.in);
         int n= sc.nextInt();
-        // printDec(n);
-        // printInc(n);
+        printDec(n);
 
     }
-    /*public static void printDec(int n){
+    public static void printDec(int n){
         if(n==0){
             return;
         }
         System.out.println(n);
         printDec(n-1);
-    }
-    public static void printInc(int n){
-        if(n==0){
-            return;
-        }
-        printInc(n-1);
-        System.out.println(n);
-        
-    }*/
 
-    //instead of calling both the functions separately, what we can do is given below
-    public static void pdi(int n){
-        System.out.println(n);
-        pdi(n-1);
-        System.out.println(n);
     }
-
     
 }
