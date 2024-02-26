eb298f21402184ebc30032ca524fee7de3357e68
Chitra2409 <107039511+Chitra2409@users.noreply.github.com>
2024-02-26 19:34:22 +0530

    Create getMazePath.java

---
+++ getMazePath.java
@@ -0,0 +1,16 @@
+//ek vertical move and ek horizontal move is allowed only at a time
+package Recursion;
+import java.util.*;
+public class getMazePath {
+    public static void main (String args[]) throws Exception{
+
+    }
+
+    //sr- source row
+    //sc- source column
+    //dr- destination row
+    //dc- destination column
+    public static ArrayList<String> getMazePaths(int sr, int sc, int dr, int dc){
+        return null;
+    }
+}
