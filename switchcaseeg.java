248e5d28aa300c82c9f9e92b8fe82747bc64a85e
Chitra2409 <107039511+Chitra2409@users.noreply.github.com>
2024-03-13 22:36:48 +0530

    Create switchcaseeg.java

---
+++ switchcaseeg.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public static double areaSwitchCase(int ch, double[] a) {
+        double area = 0.0;
+
+        switch(ch) {
+            case 1: 
+                double radius = a[0];
+                area = Math.PI * radius * radius;
+                break;
+            case 2:
+                double length = a[0];
+                double breadth = a[1];
+                area = length * breadth;
+                break;
+            default:
+                System.out.println("Invalid choice");
+        }
+
+        return area;
+    }
+}
